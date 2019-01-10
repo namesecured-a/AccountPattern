bf3f18a4682239f9e7733d650c7d47cbb03cd6e4
Sergey Psekha <pss@ciklum.com>
2019-01-10 17:49:54 +0200

    update ICommandHandler

--- src/Account.Common/Commands/ICommandHandler.cs
+++ src/Account.Common/Commands/ICommandHandler.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Account.Common.Commands
 {
-    public interface ICommandHandler
+    public interface ICommandHandler<in T> where T : ICommand
     {
+        Task HandleAsync(T command);
     }
 }
