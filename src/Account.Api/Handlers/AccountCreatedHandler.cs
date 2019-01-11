ad16b65421058f49e57690e90a0c3aa40ea66ee6
Sergey Psekha <pss@ciklum.com>
2019-01-11 16:57:18 +0200

    add AccountCreatedHandler

---
+++ src/Account.Api/Handlers/AccountCreatedHandler.cs
@@ -0,0 +1,15 @@
+﻿using Account.Common.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Account.Api.Handlers
+{
+    public class AccountCreatedHandler : IEventHandler<AccountCreated>
+    {
+        public async Task HandleAsync(AccountCreated message)
+        {   
+            await Task.CompletedTask;
+            Console.WriteLine($"Account created. Id: {message.Id}");
+        }
+    }
+}
--- src/Account.Api/Startup.cs
+++ src/Account.Api/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Account.Common.Events;
+using Account.Common.RabbitMq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,8 @@ namespace Account.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRabbitMq(this.Configuration);
+            services.AddScoped<IEventHandler<AccountCreated>, AccountCreatedHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
